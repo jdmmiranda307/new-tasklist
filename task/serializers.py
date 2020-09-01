01e8c2f6bc71f042cd5889ba1fd4ef5daad118b9
João Miranda <jvdmmiranda@gmail.com>
2020-08-31 23:26:51 -0300

    Adds to_representation to task serializer to create depth for status

--- task/serializers.py
+++ task/serializers.py
@@ -2,6 +2,13 @@ from .models import Task, Status
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Status
+        fields = '__all__'
+
+
 class TaskSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
     status = serializers.PrimaryKeyRelatedField(queryset=Status.objects.all(), required=False)
@@ -10,7 +17,7 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = ('id', 'user', 'title', 'status', 'description', 'created_at', 'updated_at', 'completed_at')
 
-class StatusSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Status
-        fields = '__all__'
+    def to_representation(self, value):
+        data = super().to_representation(value)
+        data['status'] = StatusSerializer(Status.objects.get(id=data['status'])).data
+        return data
